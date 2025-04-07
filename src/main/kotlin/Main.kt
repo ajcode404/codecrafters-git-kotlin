d7047cfda217aaaf7d72df9d0032731b7da17de2
Akash Jain <ajcodes.tech@gmail.com>
2025-04-07 18:53:16 +0000

    cat-file command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.util.zip.Inflater
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -17,8 +19,30 @@ fun main(args: Array<String>) {
         File(gitDir, "refs").mkdir()
         File(gitDir, "HEAD").writeText("ref: refs/heads/master\n")
         println("Initialized git directory")
-    } else {
+    } else if (args[0] == "cat-file") {
+        if (args[1] == "-p") {
+            val hash = args[2]
+            val gitDir = File(".git")
+            val data = decompress("${File(gitDir, "objects").absolutePath}/${hash.substring(0, 2)}/${hash.substring(2, hash.length)}")
+            val splitData = data.split('\u0000')
+            println(splitData[1])
+        }
+   } else {
         println("Unknown command: ${args[0]}")
         exitProcess(1)
     }
+}
+
+fun decompress(filepath: String): String {
+    val content = File(filepath).readBytes()
+    val inflater = Inflater()
+    val outputStream = ByteArrayOutputStream()
+    inflater.setInput(content)
+    val buffer = ByteArray(1024)
+    while (!inflater.finished()) {
+        val count = inflater.inflate(buffer)
+        outputStream.write(buffer, 0, count)
+    }
+    outputStream.close()
+    return outputStream.toString(Charsets.UTF_8)
 }
